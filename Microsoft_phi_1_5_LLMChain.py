52b0d3934796c00196f58d952e356ac8ff2efc7e
Shashikant Zarekar <zazikant@gmail.com>
2023-09-16 20:34:29 +0530

    main

--- Microsoft_phi_1_5_LLMChain.py
+++ Microsoft_phi_1_5_LLMChain.py
@@ -1,52 +1,66 @@
-#use t4 gpu in colab
+!pip -q install git+https://github.com/huggingface/transformers # need to install from github
+!pip install -q datasets loralib sentencepiece
+!pip -q install bitsandbytes accelerate xformers einops
+!pip -q install langchain
 
-#!pip -q install langchain huggingface_hub transformers sentence_transformers einops
-
-import os
-
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = 'your_token'
-
-from langchain import PromptTemplate, HuggingFaceHub, LLMChain
-
-template = """Question: {question}
-
-Answer: Let's think step by step."""
-
-prompt = PromptTemplate(template=template, input_variables=["question"])
-
-#-----------
+!nvidia-smi
 
 import torch
 import transformers
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-#-----------
-
 torch.set_default_device('cuda')
 
-#-----------
 
-from transformers import pipeline, AutoTokenizer, AutoModelForCausalLM
 
-model = AutoModelForCausalLM.from_pretrained("microsoft/phi-1_5", trust_remote_code=True, torch_dtype="auto")
-tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5", trust_remote_code=True, torch_dtype="auto")
+model = AutoModelForCausalLM.from_pretrained("microsoft/phi-1_5",
+                                             trust_remote_code=True,
+                                             torch_dtype="auto")
 
-class LLMPipeline:
-    def __init__(self, model, tokenizer):
-        self.model = model
-        self.tokenizer = tokenizer
+tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5",
+                                          trust_remote_code=True,
+                                          torch_dtype="auto")
 
-    def run(self, question):
-        inputs = self.tokenizer(question, return_tensors="pt")
-        input_ids = inputs["input_ids"]
-        outputs = self.model.generate(input_ids=input_ids, max_length=300)
-        generated_text = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
-        return generated_text
+def generate(input_text, max_new_tokens=500):
+    inputs = tokenizer(input_text, return_tensors="pt", return_attention_mask=False)
+    outputs = model.generate(**inputs, max_new_tokens=max_new_tokens)
+    text = tokenizer.batch_decode(outputs)[0]
 
-llm_chain = LLMPipeline(model=model, tokenizer=tokenizer)
+    print(text)
+    
 
-#-----------
+from langchain.chains import LLMChain, SequentialChain
+from langchain.memory import ConversationBufferMemory
+from langchain import HuggingFacePipeline
+from langchain import PromptTemplate,  LLMChain
 
-question = "write a landing page on general dentistry?"
 
-print(llm_chain.run(question))
+from transformers import AutoModel
+import torch
+import transformers
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+import json
+import textwrap
+
+
+llm = HuggingFacePipeline(pipeline=generate)
+
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+# Create a prompt template
+prompt = PromptTemplate(
+    input_variables=["product"],
+    template="What is a good name for a company that makes {product}?",
+)
+
+# Create an LLM chain
+chain = LLMChain(llm=llm, prompt=prompt)
+
+# Run the chain with user input
+user_input = "anime umbrellas"
+output = chain.run(user_input)
+
+print(output)
