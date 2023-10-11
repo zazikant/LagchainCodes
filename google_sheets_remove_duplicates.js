bbd9aac3ab51d0769b3ef63f53dce4bef1f10095
Shashikant Zarekar <zazikant@gmail.com>
2023-10-11 17:20:43 +0530

    s

--- google_sheets_remove_duplicates.js
+++ google_sheets_remove_duplicates.js
@@ -1,34 +1,34 @@
 function onEdit(e) {
-    var sheet = e.source.getSheetByName('Sheet1'); // Replace 'Sheet1' with the name of your sheet
-    var range = e.range;
-    var colIndex = range.getColumn();
-    var colName = sheet.getRange(1, colIndex).getValue();
-  
-    // Check if the edited column is 'email'
-    if (colName.toLowerCase() === 'email') {
-      var data = sheet.getDataRange().getValues();
-      var uniqueData = [];
-  
-      // Iterate through the data to find unique values based on email
-      data.forEach(function(row) {
-        var email = row[colIndex - 1]; // Assuming email is in the selected column
-        var index = uniqueData.findIndex(function(item) {
-          return item[colIndex - 1] === email;
-        });
-  
-        if (index !== -1) {
-          // If duplicate, replace the existing entry with the latest one
-          uniqueData[index] = row;
-        } else {
-          // If not a duplicate, add to the uniqueData array
-          uniqueData.push(row);
-        }
+  var sheet = e.source.getSheetByName('Sheet1'); // Replace 'Sheet1' with the name of your sheet
+  var range = e.range;
+  var colIndex = range.getColumn();
+  var colName = sheet.getRange(1, colIndex).getValue();
+
+  // Check if the edited column is 'email'
+  if (colName.toLowerCase() === 'email') {
+    var data = sheet.getDataRange().getValues();
+    var uniqueData = [];
+
+    // Iterate through the data to find unique values based on email (case-insensitive)
+    data.forEach(function(row) {
+      var email = row[colIndex - 1].toString().toLowerCase(); // Convert to lowercase
+      var index = uniqueData.findIndex(function(item) {
+        return item[colIndex - 1].toString().toLowerCase() === email;
       });
-  
-      // Clear the entire sheet
-      sheet.clearContents();
-  
-      // Write the unique data back to the sheet
-      sheet.getRange(1, 1, uniqueData.length, uniqueData[0].length).setValues(uniqueData);
-    }
+
+      if (index !== -1) {
+        // If duplicate, replace the existing entry with the latest one
+        uniqueData[index] = row;
+      } else {
+        // If not a duplicate, add to the uniqueData array
+        uniqueData.push(row);
+      }
+    });
+
+    // Clear the entire sheet
+    sheet.clearContents();
+
+    // Write the unique data back to the sheet
+    sheet.getRange(1, 1, uniqueData.length, uniqueData[0].length).setValues(uniqueData);
   }
+}
